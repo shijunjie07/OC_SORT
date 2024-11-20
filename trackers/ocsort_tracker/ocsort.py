60270e0c139ab1b97e381dd74956005d187cce2c
shijunjie07 <shijunja@yahoo.com>
2024-11-21 01:29:44 +0800

    Update ocsort.py

--- trackers/ocsort_tracker/ocsort.py
+++ trackers/ocsort_tracker/ocsort.py
@@ -212,6 +212,7 @@ class OCSort(object):
             bboxes = output_results[:, :4]  # x1y1x2y2
         img_h, img_w = img_info[0], img_info[1]
         scale = min(img_size[0] / float(img_h), img_size[1] / float(img_w))
+        bboxes.astype(np.float64)
         bboxes /= scale
         dets = np.concatenate((bboxes, np.expand_dims(scores, axis=-1)), axis=1)
         inds_low = scores > 0.1
